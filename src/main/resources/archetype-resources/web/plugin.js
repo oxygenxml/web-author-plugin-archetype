bc10da65fd8f84a1b5d4538dc1f0c97f4ea9cf3b
SYNCROSOFT\mihai_coanda <coanda.mihai@gmail.com>
2018-07-17 12:25:12 +0300

    WA-2137 Added CSS file loading in plugin.js.

--- src/main/resources/archetype-resources/web/plugin.js
+++ src/main/resources/archetype-resources/web/plugin.js
@@ -1,4 +1,12 @@
 (function () {
+  // load custom.css file
+  var cssFile;
+  cssFile = goog.dom.createDom('link');
+  cssFile.rel = "stylesheet";
+  cssFile.type = "text/css";
+  cssFile.href = "../plugin-resources/${plugin-id}/custom.css";
+  goog.dom.appendChild(document.head, cssFile);
+
   goog.events.listenOnce(workspace, sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
     console.log('Plugin loaded successfully');
   });
