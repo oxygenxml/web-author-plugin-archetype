8a4481380f316e05598b567606b5b47908b8683e
Cristian Talau <cristitalau@gmail.com>
2020-04-07 16:20:46 +0300

    Listen for all editors

--- src/main/resources/archetype-resources/web/plugin.js
+++ src/main/resources/archetype-resources/web/plugin.js
@@ -1,6 +1,6 @@
 (function () {
 	
-  goog.events.listenOnce(workspace, sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
+  goog.events.listen(workspace, sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
     console.log('Plugin loaded successfully');
   });
   
